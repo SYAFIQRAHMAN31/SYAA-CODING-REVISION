053f5af3ecb484e42d90693701a7ee75fe32b1fd
User <User@DESKTOP-2D7D7PM>
2026-09-14 10:28:27 +0800

    Add project files.

---
+++ SYAA CODING REVISION.cpp
@@ -0,0 +1,22 @@
+// SYAA CODING REVISION.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+
+void main()
+
+
+{
+    
+    int x = 100;
+    int y = 800;
+	int total = x + y;
+
+	cout << "Total is: " << total << endl;
+
+}
+
